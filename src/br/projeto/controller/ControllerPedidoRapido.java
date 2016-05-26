093425f8cb2a737c89393bccf58922a41c6bd28f
Daniel Santos <DanV8@users.noreply.github.com>
2016-05-26 19:42:46 -0300

    Melhoria na performace
    Foram modificados todos os comando que usavam o objeto estático "form" por um objeto normal, com o mesmo nome, cujas alterações são feitas antes do mesmo se tornar visível para o usuário

--- src/br/projeto/controller/ControllerPedidoRapido.java
+++ src/br/projeto/controller/ControllerPedidoRapido.java
@@ -11,7 +11,6 @@ import java.util.Locale;
 
 import javax.swing.JOptionPane;
 
-import br.projeto.model.Caixa;
 import br.projeto.model.Cliente;
 import br.projeto.model.Pizzas;
 import br.projeto.model.Refrigerante;
@@ -21,20 +20,20 @@ import br.projeto.view.FormPedidoRapido;
 public class ControllerPedidoRapido {
 	
 	/*Um dos principais controllers do projeto
-	 * SÛ abrir· se j· tiver um cliente cadastrado no sistema e se seu
+	 * S√≥ abrir√° se j√° tiver um cliente cadastrado no sistema e se seu
 	 * nome for digitado da maneira correta
 	 * 
-	 * Essa classe tem um objeto est·tico pois o mesmo objeto usa essa classe para a manipulaÁ„o de eventos
-	 * e carregar a tela com valores padr„o que ser· usada em outro controller (ControllerPrincipal)
+	 * Essa classe tem um objeto est√°tico pois o mesmo objeto usa essa classe para a manipula√ß√£o de eventos
+	 * e carregar a tela com valores padr√£o que ser√° usada em outro controller (ControllerPrincipal)
 	 * 
-	 * Sua funÁ„o È pegar o valor de constantes de enums
-	 * apresent·-los na janela, somar esses valores e armazen·-lo na vari·vel 
+	 * Sua fun√ß√£o √© pegar o valor de constantes de enums
+	 * apresent√°-los na janela, somar esses valores e armazen√°-lo na vari√°vel 
 	 * totalGasto do objeto Cliente, tirando trechos irrelevantes e, por fim
 	 * armazenar esse mesmo valor num "Caixa" do sistema
 	 * 
 	 * */
 	
-	public static FormPedidoRapido form;
+	public FormPedidoRapido form;
 	Cliente c = new Cliente();
 	DecimalFormat df = new DecimalFormat();
 	
@@ -48,7 +47,7 @@ public class ControllerPedidoRapido {
 	private NumberFormat formatoMonetario = NumberFormat.getCurrencyInstance(localeBrasil);
 	double precoSoma;
 	
-	// MÈtodo utilit·rio
+	// M√©todo utilit√°rio
 	public double parseValor(String str){
 	    Number valor = 0;
 	    try {
@@ -61,70 +60,70 @@ public class ControllerPedidoRapido {
 
 	
 	private void iniciarEventos(){ //Apresentar valor do item	
-		//Se vocÍ tem amor a sua vida, nunca mais toque em nada desse mÈtodo
+		//Se voc√™ tem amor a sua vida, nunca mais toque em nada desse m√©todo
 		
-		ControllerPedidoRapido.form.cmbPizza.addItemListener(new ItemListener() {	
+		form.cmbPizza.addItemListener(new ItemListener() {	
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				if(e.getStateChange() == ItemEvent.SELECTED){
 					Pizzas p = (Pizzas) e.getItem();
-					ControllerPedidoRapido.form.lblPrecoPizza.setText(formatoMonetario.format(p.getValue()));
+					form.lblPrecoPizza.setText(formatoMonetario.format(p.getValue()));
 				} else {
-					ControllerPedidoRapido.form.lblPrecoPizza.setText(formatoMonetario.format(0));
+					form.lblPrecoPizza.setText(formatoMonetario.format(0));
 				}
 			}
 		});
 		
-		ControllerPedidoRapido.form.cmbSuco.addItemListener(new ItemListener() {
+		form.cmbSuco.addItemListener(new ItemListener() {
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				if(e.getStateChange() == ItemEvent.SELECTED){
 					Suco s = (Suco) e.getItem();
-					ControllerPedidoRapido.form.lblPrecoSuco.setText(formatoMonetario.format(s.getValue()));
+					form.lblPrecoSuco.setText(formatoMonetario.format(s.getValue()));
 				}else{
-					ControllerPedidoRapido.form.lblPrecoSuco.setText(formatoMonetario.format(0));				}
+					form.lblPrecoSuco.setText(formatoMonetario.format(0));				}
 			}
 		});
 		
-		ControllerPedidoRapido.form.cmbRefri.addItemListener(new ItemListener() {	
+		form.cmbRefri.addItemListener(new ItemListener() {	
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				if(e.getStateChange() == ItemEvent.SELECTED){
 					Refrigerante r = (Refrigerante) e.getItem();
-					ControllerPedidoRapido.form.lblPrecoRefri.setText(formatoMonetario.format(r.getValue()));
+					form.lblPrecoRefri.setText(formatoMonetario.format(r.getValue()));
 				}else{
-					ControllerPedidoRapido.form.lblPrecoRefri.setText(formatoMonetario.format(0));
+					form.lblPrecoRefri.setText(formatoMonetario.format(0));
 				}
 			}
 		});
 		
-		ControllerPedidoRapido.form.btnCalcular.addActionListener(new ActionListener() { //Apresentar soma dos produtos
+		form.btnCalcular.addActionListener(new ActionListener() { //Apresentar soma dos produtos
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				ControllerPedidoRapido.form.precoPizza = parseValor(ControllerPedidoRapido.form.lblPrecoPizza.getText());
-				ControllerPedidoRapido.form.precoSuco = parseValor(ControllerPedidoRapido.form.lblPrecoSuco.getText());
-				ControllerPedidoRapido.form.precoRefri = parseValor(ControllerPedidoRapido.form.lblPrecoRefri.getText());
-				precoSoma = ControllerPedidoRapido.form.precoPizza 
-						+ ControllerPedidoRapido.form.precoSuco + ControllerPedidoRapido.form.precoRefri; 
+				form.precoPizza = parseValor(form.lblPrecoPizza.getText());
+				form.precoSuco = parseValor(form.lblPrecoSuco.getText());
+				form.precoRefri = parseValor(form.lblPrecoRefri.getText());
+				precoSoma = form.precoPizza 
+						+ form.precoSuco + form.precoRefri; 
 				
-				ControllerPedidoRapido.form.txtPrecoTotal.setText(formatoMonetario.format(precoSoma));
+				form.txtPrecoTotal.setText(formatoMonetario.format(precoSoma));
 				
-				if(ControllerPedidoRapido.form.chkVip.isSelected()){
+				if(form.chkVip.isSelected()){
 					double desconto = (precoSoma)*0.15;
 					precoSoma -= desconto;
-					ControllerPedidoRapido.form.txtPrecoTotal.setText(formatoMonetario.format(precoSoma));
+					form.txtPrecoTotal.setText(formatoMonetario.format(precoSoma));
 				}
-				ControllerPedidoRapido.form.btnConfirmar.setEnabled(true);
+				form.btnConfirmar.setEnabled(true);
 			}
 		});
 		
-		ControllerPedidoRapido.form.btnConfirmar.addActionListener(new ActionListener() {
+		form.btnConfirmar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				ControllerPrincipal.caixa.setPrecoTotal(ControllerPrincipal.caixa.getPrecoTotal() +
 						precoSoma);
-				ControllerPedidoRapido.form.setVisible(false);
+				form.setVisible(false);
 				JOptionPane.showMessageDialog(null ,"Pedido realizado com sucesso !");
 			}
 		});
@@ -134,13 +133,13 @@ public class ControllerPedidoRapido {
 	private void carregarTela(){ //Carregar JComboBox com Enums
 		
 		for(Pizzas pizza : Pizzas.values()){
-			ControllerPedidoRapido.form.cmbPizza.addItem(pizza);
+			form.cmbPizza.addItem(pizza);
 		}
 		for(Suco suco : Suco.values()){
-			ControllerPedidoRapido.form.cmbSuco.addItem(suco);
+			form.cmbSuco.addItem(suco);
 		}
 		for(Refrigerante refri : Refrigerante.values()){
-			ControllerPedidoRapido.form.cmbRefri.addItem(refri);
+			form.cmbRefri.addItem(refri);
 		}
 	}
 	
