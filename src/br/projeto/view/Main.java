99aaaf94f190bca43afe92d5f80af9381f351600
Daniel Santos <DanV8@users.noreply.github.com>
2016-05-26 19:51:02 -0300

    Update do programa
    A versão 1.1 do programa:
    - Melhora a legibilidade do código;
    - Melhora a perfomace eliminando linhas e métodos irrelevantes;
    - Adiciona mais duas classes ao projeto;

--- src/br/projeto/view/Main.java
+++ src/br/projeto/view/Main.java
@@ -7,13 +7,10 @@ public class Main {
 	/**
 	 * @author Daniel
 	 * @since 2016/03/01
-	 * @version 1.0
-	 * @serialData 2016/05/02
+	 * @version 1.1
+	 * @serialData 2016/05/26
 	 * */
 	
-	/*Problemas a resolver:
-	 * linha 52 do controller principal;
-	 * validar campos ignorando caracteres da mascara*/
 	public static void main(String[] args) {
 		new ControllerPrincipal();
 	}
