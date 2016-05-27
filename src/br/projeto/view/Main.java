9d328c6ddc8dfcebd975571e9d7ec900f431e28c
Daniel Santos <DanV8@users.noreply.github.com>
2016-05-27 18:02:26 -0300

    Update do serialdata

--- src/br/projeto/view/Main.java
+++ src/br/projeto/view/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	 * @author Daniel
 	 * @since 2016/03/01
 	 * @version 1.1
-	 * @serialData 2016/05/26
+	 * @serialData 2016/05/27
 	 * */
 	
 	public static void main(String[] args) {
