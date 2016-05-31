b710bd31379e12dd3542177399c6e7674a57c8ef
Daniel Santos <DanV8@users.noreply.github.com>
2016-05-31 14:59:06 -0300

    Update ControllerValidacao.java

--- src/br/projeto/controller/ControllerValidacao.java
+++ src/br/projeto/controller/ControllerValidacao.java
@@ -8,13 +8,18 @@ import br.projeto.view.FormValidacao;
 
 public class ControllerValidacao {
 	
-	/*Essa janela ser· a transiÁ„o entre a janela principal do programa e 
-	 * a o formul·rio de pedido, que sÛ ser· aberto se o nome do cliente
+	/**
+	 * @author Daniel
+	 * @see FormValidacao
+	 * @see ControllerPedidoRapido
+	 * 
+	 * Essa janela ser√° a transi√ß√£o entre a janela principal do programa e 
+	 * a o formul√°rio de pedido, que s√≥ ser√° aberto se o nome do cliente
 	 * estiver nos registros do sistema
 	 * 
-	 * Essa janela ser· aberta sempre que o usu·rio clicar em "Pedido R·pido"
-	 * Sua funÁ„o È validar se o cliente existe, em caso positivo, a mesma
-	 * chamar· a jenla de pedido, em caso negativo, n„o acontecer· nada (previs„o de mudanÁa na vers„o 1.2)*/
+	 * Essa janela ser√° aberta sempre que o usu√°rio clicar em "Pedido R√°pido"
+	 * Sua fun√ß√£o √© validar se o cliente existe, em caso positivo, a mesma
+	 * chamar√° a jenla de pedido, em caso negativo, n√£o acontecer√° nada (previs√£o de mudan√ßa na vers√£o 1.2)*/
 
 	FormValidacao form;
 	Cliente c;
@@ -29,19 +34,26 @@ public class ControllerValidacao {
 	}
 	
 	private void iniciarEventos(){
+		/**
+		 * @author Daniel
+		 * 
+		 * Esse m√©todo ser√° chamado sempre que o usu√°rio clicar no bot√£o Ok 
+		 * para validar a condi√ß√£o do cliente*/
+		
 		form.btnOk.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				c.setNome(form.txtNome.getText()); //pegar nome para a validaÁ„o
+				c.setNome(form.txtNome.getText()); //pegar nome para a valida√ß√£o
 				
 				for (int i = 0; i < ControllerCadastro.clienteDB.size(); i++) {
-					/*Nesse caso, n„o utilizamos um foreach pois precisamos pegar o cliente de 
-					 * determinado Ìnidce para setar seus dados no cabeÁalho da janela de pedido*/
+					/* Nesse caso, n√£o utilizamos um foreach pois precisamos pegar o cliente de 
+					 * determinado √≠nidce para setar seus dados no cabe√ßalho da janela de pedido*/
 					
 					if(c.equals(ControllerCadastro.clienteDB.get(i))){
 						/*A propriedade "equals" compara apenas o nome do cliente, de acordo com a 
 						 * sobrescrita do mesmo na classe "Cliente"*/
+						
 						c = ControllerCadastro.clienteDB.get(i);
 						formPR.form.setVisible(true);
 						formPR.form.lblNome.setText(c.getNome());
